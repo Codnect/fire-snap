cd5599cc094254ef4524379fc24bb58caa8e76ad
Burak Koken <burakkokenn@gmail.com>
2018-09-30 23:46:21 +0200

    Created a base class  to bind the properties.

---
+++ app/src/main/java/org/codnect/firesnap/binder/PropertyBinder.java
@@ -0,0 +1,42 @@
+package org.codnect.firesnap.binder;
+
+import org.codnect.firesnap.annotation.AccessType;
+import org.codnect.firesnap.core.InheritanceState;
+import org.codnect.firesnap.core.MetadataContext;
+import org.codnect.firesnap.mapping.PropertyHolder;
+import org.codnect.firesnap.reflection.XClass;
+
+import java.util.Map;
+
+/**
+ * Created by Burak Koken on 30.9.2018.
+ *
+ * @author Burak Koken
+ */
+public class PropertyBinder {
+
+    private String propertyName;
+    private AccessType accessType;
+    private XClass declaringClass;
+    private ModelBinder modelBinder;
+    private PropertyHolder propertyHolder;
+    private Map<XClass, InheritanceState> inheritanceStateMap;
+    private MetadataContext metadataContext;
+
+    public PropertyBinder(String propertyName,
+                          AccessType accessType,
+                          XClass declaringClass,
+                          ModelBinder modelBinder,
+                          PropertyHolder propertyHolder,
+                          Map<XClass, InheritanceState> inheritanceStateMap,
+                          MetadataContext metadataContext) {
+        this.propertyName = propertyName;
+        this.accessType = accessType;
+        this.declaringClass = declaringClass;
+        this.modelBinder = modelBinder;
+        this.propertyHolder = propertyHolder;
+        this.inheritanceStateMap = inheritanceStateMap;
+        this.metadataContext = metadataContext;
+    }
+
+}
