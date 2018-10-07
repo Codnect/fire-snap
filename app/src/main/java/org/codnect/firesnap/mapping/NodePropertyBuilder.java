262582d284678d7f23401b757ce997a02695f6bb
Burak Koken <burakkokenn@gmail.com>
2018-10-07 23:46:15 +0200

    Created the NodePropertyBuilder class to handle the Property annotation.

---
+++ app/src/main/java/org/codnect/firesnap/mapping/NodePropertyBuilder.java
@@ -0,0 +1,44 @@
+package org.codnect.firesnap.mapping;
+
+import org.codnect.firesnap.annotation.Property;
+import org.codnect.firesnap.binder.ModelBinder;
+import org.codnect.firesnap.core.MetadataContext;
+import org.codnect.firesnap.core.PropertyHolder;
+import org.codnect.firesnap.reflection.XProperty;
+
+/**
+ * Created by Burak Koken on 7.10.2018.
+ *
+ * @author Burak Koken
+ */
+public class NodePropertyBuilder {
+
+    private XProperty property;
+    private PropertyData propertyData;
+    private PropertyHolder propertyHolder;
+    private ModelBinder modelBinder;
+    private MetadataContext metadataContext;
+
+    public NodePropertyBuilder(XProperty property,
+                               PropertyData propertyData,
+                               PropertyHolder propertyHolder,
+                               ModelBinder modelBinder,
+                               MetadataContext metadataContext) {
+        this.property = property;
+        this.propertyData = propertyData;
+        this.propertyHolder = propertyHolder;
+        this.modelBinder = modelBinder;
+        this.metadataContext = metadataContext;
+    }
+
+    /**
+     *
+     */
+    public void build() {
+        if(property.isAnnotationPresent(Property.class)) {
+            Property propertyAnnotation = property.getAnnotation(Property.class);
+        }
+        /* code */
+    }
+
+}
